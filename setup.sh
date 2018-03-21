5c427ec04b99386fa9c63466cd8c24c4686ba4c4
Thomas COUCHOUD <zerderr@gmail.com>
2018-03-21 15:51:01 +0100

    Try

--- setup.sh
+++ setup.sh
@@ -1,6 +1,29 @@
 #!/bin/sh
 sudo apt-get update
-sudo apt-get install libibmad-dev libibumad-dev ibsim-utils opensm git gcc g++ python python-dev mercurial bzr gdb valgrind gsl-bin libgsl0-dev libgsl0ldbl flex bison tcpdump sqlite sqlite3 libsqlite3-dev libxml2 libxml2-dev libgtk2.0-0 libgtk2.0-dev uncrustify doxygen graphviz imagemagick texlive texlive-latex-extra texlive-generic-extra texlive-generic-recommended texinfo dia texlive texlive-latex-extra texlive-extra-utils texlive-generic-recommended texi2html python-pygraphviz python-kiwi python-pygoocanvas libgoocanvas-dev python-pygccxml
+sudo apt-get install libibmad-dev
+sudo apt-get install libibumad-dev
+sudo apt-get install ibsim-utils
+sudo apt-get install opensm
+sudo apt-get install git
+sudo apt-get install gcc
+sudo apt-get install g++
+sudo apt-get install python
+sudo apt-get install python-dev
+sudo apt-get install mercurial
+sudo apt-get install bzr
+sudo apt-get install gdb
+sudo apt-get install valgrind
+sudo apt-get install gsl-bin
+#sudo apt-get install libgsl0-dev
+#sudo apt-get install libgsl0ldbl 
+sudo apt-get install libgsl2-dev
+sudo apt-get install libgsl2
+sudo apt-get install flex
+sudo apt-get install bison
+sudo apt-get install tcpdump
+sudo apt-get install sqlite
+sudo apt-get install sqlite3
+sudo apt-get install libsqlite3-dev libxml2 libxml2-dev libgtk2.0-0 libgtk2.0-dev uncrustify doxygen graphviz imagemagick texlive texlive-latex-extra texlive-generic-extra texlive-generic-recommended texinfo dia texlive texlive-latex-extra texlive-extra-utils texlive-generic-recommended texi2html python-pygraphviz python-kiwi python-pygoocanvas libgoocanvas-dev python-pygccxml
 git clone https://github.com/jgunthorpe/ibsim.git
 cd ibsim
 make
